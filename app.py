9aaf14b00f6f2baa6196fd316685b97e54dfe76f
Margareta Valencia <margaretavalencia16@gmail.com>
2024-12-17 20:02:17 +0700

    done

--- app.py
+++ app.py
@@ -50,12 +50,12 @@ def predict_pollution(model, pm10, pm25, so2, co, o3, no2):
 
 def map_pollution_level(prediction):
     pollution_levels = {
-        0: ('Baik', 'Air quality is good and poses little or no risk.', 'green'),
-        1: ('Sedang', 'Air quality is acceptable; however, some pollutants may cause a health concern for sensitive individuals.', 'blue'),
-        2: ('Tidak Sehat', 'Everyone may begin to experience health effects; sensitive groups may experience more serious effects.', 'yellow'),
-        3: ('Sangat Tidak Sehat', 'Health alert: everyone may experience more serious health effects.', 'red')
+        0: ('Baik', 'Tingkat mutu udara yang sangat baik, tidak memberikan efek negatif terhadap manusia, hewan, dan tumbuhan.', 'green'),
+        1: ('Sedang', 'Tingkat mutu udara masih dapat diterima pada kesehatan manusia, hewan, dan tumbuhan.', 'blue'),
+        2: ('Tidak Sehat', 'Tingkat mutu udara yang bersifat merugikan pada manusia, hewan, dan tumbuhan.', 'yellow'),
+        3: ('Sangat Tidak Sehat', 'Tingkat mutu udara yang dapat meningkatkan resiko kesehatan pada sejumlah segmen populasi yang terpapar.', 'red')
     }
-    return pollution_levels.get(prediction, ("Unknown", "No description available.", "gray"))
+    return pollution_levels.get(prediction, ("Tidak Diketahui", "Tidak ada deskripsi yang sesuai.", "gray"))
 
 def app():
     # Page title
@@ -65,13 +65,13 @@ def app():
     # Input fields for pollutant data
     col1, col2 = st.columns(2)
     with col1:
-        pm10 = st.number_input('PM10 (µg/m³)', min_value=0.0)
-        pm25 = st.number_input('PM2.5 (µg/m³)', min_value=0.0)
-        so2 = st.number_input('SO2 (ppm)', min_value=0.0)
+        pm10 = st.number_input('PM10 (µg/m³)', min_value=0.0, format="%.3f")
+        pm25 = st.number_input('PM2.5 (µg/m³)', min_value=0.0, format="%.3f")
+        so2 = st.number_input('SO2 (ppm)', min_value=0.0, format="%.3f")
     with col2:
-        co = st.number_input('CO (ppm)', min_value=0.0)
-        o3 = st.number_input('O3 (ppm)', min_value=0.0)
-        no2 = st.number_input('NO2 (ppm)', min_value=0.0,)
+        co = st.number_input('CO (ppm)', min_value=0.0, format="%.3f")
+        o3 = st.number_input('O3 (ppm)', min_value=0.0, format="%.3f")
+        no2 = st.number_input('NO2 (ppm)', min_value=0.0,  format="%.3f")
 
     # Model selection
     selected_model_name = st.selectbox('Select a model', list(models.keys()))
@@ -79,6 +79,14 @@ def app():
     # Load the selected model
     model_filename = models[selected_model_name]
     classifier = load_model(model_filename)
+
+    # Define the prediction function
+    def predict_pollution(pm10, pm25, so2,  co, o3, no2):
+        if classifier is not None:
+            prediction = classifier.predict([[pm10, pm25, so2,  co, o3, no2]])
+            return prediction
+        else:
+            raise ValueError("The classifier model is not loaded.")
 
     # Prediction button
     if st.button('Predict'):
