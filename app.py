b9f4ad8e885698544955372ebdd40b1a561a577c
Margareta Valencia <113288412+margaretavalen@users.noreply.github.com>
2024-12-31 18:30:46 +0000

    done

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 import pickle
 import os
 
 # Streamlit page configuration
 st.set_page_config(
     page_title="Air Pollution Prediction",
+    page_icon="💨",
+    layout="wide",      
     menu_items={
         "Get Help": "https://github.com/kedarnathdev/AQIprediction",
         "Report a bug": "https://github.com/kedarnathdev/AQIprediction/issues",
@@ -60,45 +63,59 @@ def map_pollution_level(prediction):
     return pollution_levels.get(prediction, ("Tidak Diketahui", "Tidak ada deskripsi yang sesuai.", "gray"))
 
 def app():
-    # Page title
-    st.header('Air Quality Index Prediction')
-    st.subheader('Enter the following pollutant values:')
+    # Navigasi Sidebar
+    with st.sidebar :
+     selected = option_menu ('AIR QUALITY INDEX',
+    ['Home',
+    'Data',
+    'Prediction',
+    'Visualization'],
+    default_index=0)
 
-    # Input fields for pollutant data
-    col1, col2 = st.columns(2)
-    with col1:
-        pm10 = st.number_input('PM10 (µg/m³)', min_value=0.0, max_value=600.0, format="%.3f")
-        pm25 = st.number_input('PM2.5 (µg/m³)', min_value=0.0, max_value=500.0, format="%.3f")
-        so2 = st.number_input('SO2 (ppm)', min_value=0.0, max_value=1.0, format="%.3f")
-    with col2:
-        co = st.number_input('CO (ppm)', min_value=0.0, max_value=2.0, format="%.3f")
-        o3 = st.number_input('O3 (ppm)', min_value=0.0, max_value=0.6, format="%.3f")
-        no2 = st.number_input('NO2 (ppm)', min_value=0.0,  max_value=50.0, format="%.3f")
+    if (selected == 'Home') :
+        st.title('EJAAYAP')
 
-    st.write(f"Input data: PM10={pm10}, PM2.5={pm25}, SO2={so2}, CO={co}, O3={o3}, NO2={no2}")
 
-    # Model selection
-    selected_model_name = st.selectbox('Select a model', list(models.keys()))
+    if (selected == 'Prediction') :
+        # Page title
+        st.header('Air Quality Index Prediction')
+        st.subheader('Enter the following pollutant values:')
 
-    # Load the selected model
-    model_filename = models[selected_model_name]
-    classifier = load_model(model_filename)
+        # Input fields for pollutant data
+        col1, col2 = st.columns(2)
+        with col1:
+            pm10 = st.number_input('PM10 (µg/m³)', min_value=0.0, max_value=600.0, format="%.3f")
+            pm25 = st.number_input('PM2.5 (µg/m³)', min_value=0.0, max_value=500.0, format="%.3f")
+            so2 = st.number_input('SO2 (ppm)', min_value=0.0, max_value=1.0, format="%.3f")
+        with col2:
+            co = st.number_input('CO (ppm)', min_value=0.0, max_value=2.0, format="%.3f")
+            o3 = st.number_input('O3 (ppm)', min_value=0.0, max_value=0.6, format="%.3f")
+            no2 = st.number_input('NO2 (ppm)', min_value=0.0,  max_value=50.0, format="%.3f")
 
-    # Define the prediction function
-    def predict_pollution(model, pm10, pm25, so2,  co, o3, no2):
-        if classifier is not None:
-            prediction = classifier.predict([[pm10, pm25, so2,  co, o3, no2]])
-            return prediction
-        else:
-            raise ValueError("The classifier model is not loaded.")
+        st.write(f"Input data: PM10={pm10}, PM2.5={pm25}, SO2={so2}, CO={co}, O3={o3}, NO2={no2}")
 
-    # Prediction button
-    if st.button('Predict'):
-        prediction = predict_pollution(classifier, pm10, pm25, so2, co, o3, no2)
-        if prediction is not None:
-            pollution_level, description, color = map_pollution_level(prediction[0])
-            st.markdown(f"<h4 style='color:{color}'>The predicted air quality category is: {pollution_level}</h4>", unsafe_allow_html=True)
-            st.info(description)
+        # Model selection
+        selected_model_name = st.selectbox('Select a model', list(models.keys()))
+
+        # Load the selected model
+        model_filename = models[selected_model_name]
+        classifier = load_model(model_filename)
+
+        # Define the prediction function
+        def predict_pollution(model, pm10, pm25, so2,  co, o3, no2):
+            if classifier is not None:
+                prediction = classifier.predict([[pm10, pm25, so2,  co, o3, no2]])
+                return prediction
+            else:
+                raise ValueError("The classifier model is not loaded.")
+
+        # Prediction button
+        if st.button('Predict'):
+            prediction = predict_pollution(classifier, pm10, pm25, so2, co, o3, no2)
+            if prediction is not None:
+                pollution_level, description, color = map_pollution_level(prediction[0])
+                st.markdown(f"<h4 style='color:{color}'>The predicted air quality category is: {pollution_level}</h4>", unsafe_allow_html=True)
+                st.info(description)
 
 if __name__ == "__main__":
     app()
